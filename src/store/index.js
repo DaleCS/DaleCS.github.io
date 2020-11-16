34a7d2106afaf2ae24946035089688f3266493ef
DaleCS <Dale.Seen@gmail.com>
2020-11-16 15:39:42 -0800

    Updated resume

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 export { default as projects } from "./projects.json";
 
-export { default as ResumePDF } from "./DCS Public Resume 2020.pdf";
+export { default as ResumePDF } from "./DaleCS Public Resume 2020.pdf";
 
 export { default as ResumeIcon } from "./icons/resume_64.svg";
 export { default as EmailIcon } from "./icons/email_64.svg";
